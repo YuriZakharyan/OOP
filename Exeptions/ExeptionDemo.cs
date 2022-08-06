9afbdd309df8581865b74ea2347b713b348722e8
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-08-07 01:10:57 +0400

    try/catch

---
+++ Exeptions/ExeptionDemo.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exeptions
+{
+    class ExeptionDemo
+    {
+        public int GrandParentGetNumber(int position)
+        {
+            return ParentGetNumber(position);
+        }
+        public int ParentGetNumber(int position)
+        {
+            return GetNumber(position);
+        }
+        public int GetNumber(int position)
+        {
+            int output = 0;
+            //try
+            //{
+                int[] arr = new int[] { 1, 2, 3, 4 };
+                output = arr[position];
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+            return output;
+        }
+    }
+}
---
+++ Exeptions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Exeptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    ExeptionDemo ob = new ExeptionDemo();
+            //    Console.WriteLine(ob.GrandParentGetNumber(9));
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+
+
+        }
+    }
+}
