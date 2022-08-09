ee06259313c514251ee3a059621c9e2a663098b2
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-08-10 01:55:51 +0400

    Delegates......

---
+++ Delegates/DelegIntroClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public delegate T MyDelegate<T>(T a, T b);
+    class DelegIntroClass
+    {
+
+        //target method
+        public static T Sum<T>(T a, T b)
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return a1 + b1;
+        }
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyDelegate del = new MyDelegate(DelegIntroClass.Print);
+            //del.Invoke("Hello World");
+            //
+            MyDelegate<int> del1 = new MyDelegate<int>(DelegIntroClass.Sum);
+            del1 += DelegIntroClass.Sum;
+            Console.WriteLine(del1(3, 6));
+            del1 += DelegIntroClass.Mul;
+            Console.WriteLine(del1(3, 6));
+            del1 += DelegIntroClass.Sum;
+            Console.WriteLine(del1(3, 6));
+            del1 += DelegIntroClass.Sub;
+            Console.WriteLine(del1(3, 6));
+
+
+        }
+    }
+}
