8453644a7a8126c195b2e083b1e8406f604fa584
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-08-17 20:40:36 +0400

    Write/Read Excel

---
+++ Excel_Example/ReadExcel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace Excel_Example
+{
+    class ReadExcel
+    {
+        public static void readExcel()
+        {
+
+            string filePath = @"D:\Names_Basic.xlsx";
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook wb;
+            Worksheet ws;
+
+            wb = excel.Workbooks.Open(filePath);
+            ws = wb.Worksheets[1];
+            Range cell = ws.Range["A1:A1"];
+            foreach (var Result in cell.Value)
+            {
+                MessageBox.Show(Result);
+            }
+            wb.Close();
+
+        }
+    }
+}
---
+++ Excel_Example/WriteExcel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace Excel_Example
+{
+    class WriteExcel
+    {
+        public static void writeExcel()
+        {
+            string filePath = @"D:\Names_Basic.xlsx";
+            Application excel = new Application();
+            Workbook wb;
+            Worksheet ws;
+            
+            wb = excel.Workbooks.Open(filePath);
+            ws = wb.Worksheets[1];
+
+            Range cellRange = ws.Range["C2:D3"];
+            cellRange.Value = "Aram";
+
+            wb.SaveAs(filePath);
+            wb.Close();
+
+        }
+    }
+}
