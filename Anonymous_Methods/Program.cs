6e94beac7de111b23e69f3a2c52dad2c735ac563
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-08-30 22:08:10 +0400

    Amonymous methods

---
+++ Anonymous_Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Anonymous_Methods
+{
+    class Program
+    {
+        public delegate void Print(int value);
+        public delegate int Calc(int value);
+
+        static void Main(string[] args)
+        {
+
+            Print print = delegate (int value)
+            {
+                Console.WriteLine($"Value: {value}");
+            };
+            print(100);
+
+
+
+            Calc fact = delegate (int value)
+            {
+                int fact = 1;
+                while (value != 0)
+                {
+                    fact *= value;
+                    value--;
+                }
+                return fact;
+            };
+            Console.WriteLine(fact(3));
+
+
+
+
+        }
+    }
+}
