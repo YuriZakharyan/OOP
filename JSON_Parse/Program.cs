aa6726285d49730e48ec8ed718c06cc3242f0138
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-09-02 08:16:36 +0400

    JSON

---
+++ JSON_Parse/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JSON_Parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student()
+            {
+                ID = 100,
+                Name = "Yuri",
+                Age = 21,
+                Hobbies = new List<string>()
+                {
+                    "Reading",
+                    "Programming"
+                }
+            };
+            Student student2 = new Student()
+            {
+                ID = 101,
+                Name = "Harut",
+                Age = 23,
+                Hobbies = new List<string>()
+                {
+                    "Reading",
+                    "Programming"
+                }
+            };
+
+            string text1 = JsonConvert.SerializeObject(student1);       //Parse json
+            string text2 = JsonConvert.SerializeObject(student2);
+            Console.WriteLine(text1);
+            Console.WriteLine(text2);
+
+            File.WriteAllText(@"D:\Yuri.txt", text1);       //write json in .txt file 
+            File.WriteAllText(@"D:\Harut.txt", text2);
+
+        }
+    }
+}
---
+++ JSON_Parse/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Parse
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Hobbies { get; set; }
+
+
+    }
+}
